d1caed9a2d272eefe826d453484c14956ceca7ac
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 17:10:26 -0600

    commit inicial
    Se estructura de carpetas para la funcionalidad de lector,QR

---
+++ QrScannerE/Features/QRScanner/Presentation/QRScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  QRScannerView.swift
+//  QrScannerE
+//
+//  Created by David Villegas Santana on 05/03/25.
+//
+
+import SwiftUI
+
+struct QRScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScannerView()
+}
--- QrScannerE/QrScannerEApp.swift
+++ QrScannerE/QrScannerEApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct QrScannerEApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            QRScannerView()
         }
     }
 }
