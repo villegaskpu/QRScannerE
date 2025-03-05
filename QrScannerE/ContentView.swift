1928b38003cb8b4874621289e20910b4c2e966d3
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 17:02:47 -0600

    Initial Commit

---
+++ QrScannerE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QrScannerE
+//
+//  Created by David Villegas Santana on 05/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QrScannerE/QrScannerEApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrScannerEApp.swift
+//  QrScannerE
+//
+//  Created by David Villegas Santana on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct QrScannerEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
