8cd7c27087f5cfcf8412bfcc934f933b8614f288
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 17:51:27 -0600

    se crea pantalla donde se mostrara el escaner del Qr

--- QrScannerE/Features/QRScanner/Presentation/QRScannerView.swift
+++ QrScannerE/Features/QRScanner/Presentation/QRScannerView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct QRScannerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack  {
+            scanningOverlay
+            scanResultView
+        }
+    }
+    
+    private var scanningOverlay: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width: CGFloat = min(geometry.size.width, geometry.size.height) * 0.7
+                let height = width
+                let x = (geometry.size.width - width) / 2
+                let y = (geometry.size.height - height) / 2
+                
+                path.addRect(CGRect(x: x, y: y, width: width, height: height))
+            }
+            .stroke(Color.green, lineWidth: 2)
+        }
+    }
+    
+    private var scanResultView: some View {
+        Text("Texto del QR:")
+            .font(.headline)
     }
 }
 
