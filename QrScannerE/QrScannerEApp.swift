cc011e2ffe688b6d22ccdc978516c86449a73db6
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 18:50:18 -0600

    se inyectan los servicios para el viewmodel del escaner

--- QrScannerE/QrScannerEApp.swift
+++ QrScannerE/QrScannerEApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct QrScannerEApp: App {
+    
+    @StateObject private var qrVM = QRScannerViewModel(
+        scannerService: QRScannerService(),
+        storageService: QRStorageService()
+    )
+    
+    
     var body: some Scene {
         WindowGroup {
             QRScannerView()
+                .environmentObject(qrVM)
+                .transition(.slide)
         }
     }
 }
