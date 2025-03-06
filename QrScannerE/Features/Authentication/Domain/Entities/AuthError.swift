077297a3001a9939a0674ebf8afc98ee2694d867
david villegas santana <davidvillegas14@gmail.com>
2025-03-06 13:16:23 -0600

    se establesen los errores con mejor descripción

--- QrScannerE/Features/Authentication/Domain/Entities/AuthError.swift
+++ QrScannerE/Features/Authentication/Domain/Entities/AuthError.swift
@@ -7,8 +7,35 @@
 
 import Foundation
 
-enum AuthError: Error {
+enum AuthError: Error, Equatable {
+    case pinNotSet
+    case pinsDontMatch
     case biometricNotAvailable
     case authenticationFailed
     case invalidPIN
+    case invalidPINFormat
+    case secureStorageError(SecureStorageService.SecureStorageError)
+    case unknown
+    
+    
+    var localizedDescription: String {
+        switch self {
+        case .biometricNotAvailable:
+            return "Autenticación biométrica no disponible"
+        case .authenticationFailed:
+            return "Error de autenticación"
+        case .invalidPIN:
+            return "PIN no válido"
+        case .pinNotSet:
+            return "No hay PIN registrado"
+        case .invalidPINFormat:
+            return "El PIN debe tener al menos 6 dígitos"
+        case .secureStorageError(let error):
+            return "Error de seguridad: \(error.localizedDescription)"
+        case .pinsDontMatch:
+            return "Los códigos no coinciden"
+        case .unknown:
+            return "Error desconocido"
+        }
+    }
 }
