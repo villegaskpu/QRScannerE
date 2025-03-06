72acb03c28ffa56a3fac104065a19047e013d4e6
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 18:13:52 -0600

    se crea viewmodel para la vista del scaner

---
+++ QrScannerE/Features/QRScanner/Presentation/QRScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  QRScannerViewModel.swift
+//  QrScannerE
+//
+//  Created by David Villegas Santana on 05/03/25.
+//
+import SwiftUI
+import AVFoundation
+
+@MainActor
+final class QRScannerViewModel: ObservableObject {
+    func startScanning() {
+        
+    }
+    
+    func stopScanning() {
+        
+    }
+    
+    func resetScanner() {
+        
+    }
+}
