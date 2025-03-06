e0fe1a85f258975626adb1485bdcb3ca978e12ce
david villegas santana <davidvillegas14@gmail.com>
2025-03-05 18:57:38 -0600

    se crea vista para los biometricos

---
+++ QrScannerE/Features/Authentication/Presentation/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  QrScannerE
+//
+//  Created by David Villegas Santana on 05/03/25.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
