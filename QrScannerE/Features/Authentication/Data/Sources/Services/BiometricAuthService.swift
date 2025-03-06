ae234bf49710bf3332c2ff4bf4c9e42070fdcf9e
david villegas santana <davidvillegas14@gmail.com>
2025-03-06 13:18:22 -0600

    se agregan los metodos pata la autenticación de  LocalAuthentication

--- QrScannerE/Features/Authentication/Data/Sources/Services/BiometricAuthService.swift
+++ QrScannerE/Features/Authentication/Data/Sources/Services/BiometricAuthService.swift
@@ -5,5 +5,28 @@
 //  Created by David Villegas Santana on 05/03/25.
 //
 
+import LocalAuthentication
+
 final class BiometricAuthService {
+    private let context = LAContext()
+    private var error: NSError?
+    
+    var isBiometricAvailable: Bool {
+        context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
+    }
+    
+    func authenticate() async throws -> Bool {
+        try await withCheckedThrowingContinuation { continuation in
+            context.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: "Autentícate para acceder"
+            ) { success, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                } else {
+                    continuation.resume(returning: success)
+                }
+            }
+        }
+    }
 }
